843cc125be3a733232fc3d05e50f6dd5eb0c13de
Brochu <alexbrochu1@gmail.com>
2024-01-15 00:41:30 -0500

    WIP: Progress on field poly gen. Got the vertices data, just need the indices next

--- game/Field/FieldSpawner.cs
+++ game/Field/FieldSpawner.cs
@@ -1,27 +1,37 @@
 using Godot;
-using System;
+using Godot.Collections;
 
 using game.Field;
 
 public partial class FieldSpawner : Polygon2D
 {
-	static private Vector2I sceneSize = new Vector2I(1150, 645);
+	static private Vector2I sceneSize = new Vector2I(1154, 648);
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		GD.Print(string.Format("[FIELD-SPAWN] screen dimensions: ({0}, {1})", sceneSize.X, sceneSize.Y));
-
-		foreach(float f in FieldState.heightmap) {
-			GD.Print(string.Format("[FIELD-SPAWN] value = {0}", f));
-		}
 		int step = sceneSize.X / FieldState.heightmap.Length;
-		GD.Print(string.Format("[FIELD-SPAWN] step = {0}", step));
 
+		Array<int> samples = new Array<int>();
 		for(int i = 0; i <= sceneSize.X; i += step) {
-			GD.Print(string.Format("[FIELD-SPAWN] Sampling at X = {0}", i));
-			//TODO: Generate sub-polygon for this slice of the screen
+			samples.Add(i);
 		}
+
+		Array<Vector2> verts = new Array<Vector2>();
+		for(int i = 1; i < samples.Count; i++) {
+			verts.Add(new Vector2I(samples[i], 200));
+			verts.Add(new Vector2I(samples[i], sceneSize.Y));
+		}
+		GD.Print("[POLY] count = ", verts.Count);
+		
+		for (int i = 0; i < verts.Count - 2; i+=2) {
+			GD.Print(string.Format("[POLYs] ({0}); ({1}); ({2}); ({3})",
+						i, i+2, i+3, i+1));
+		}
+
+		Array<Array<int>> polys = new Array<Array<int>>();
+		//this.Polygons = [];
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
