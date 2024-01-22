ac792132b7fbff7a58d55fef11ad1fa89368e07b
Alex Brochu <alexbrochu1@gmail.com>
2024-01-21 19:19:17 -0500

    WIP: Progress on enemy hit FX

--- game/Bread/SplutFX.cs
+++ game/Bread/SplutFX.cs
@@ -3,26 +3,40 @@ using Godot;
 public partial class SplutFX : Node2D
 {
 	[Export] private Label lbl;
-	[Export] private Curve scale;
+	[Export] private Curve curve0;
 	[Export] private float maxTime = 1.0f;
 
 	private bool isRunning = false;
+	private ulong startTime = 0;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		GD.Print("Got the label : ", lbl.Name);
+		lbl.Visible = false;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		if (isRunning)
+		{
+			ulong elapsed = System.Math.Min(Time.GetTicksMsec() - startTime, (ulong)(1000 * maxTime));
+			float p = elapsed / (float)(1000 * maxTime);
+			float val = curve0.Sample(p);
+			//GD.Print("e = ", elapsed, " p = ", p, " val = ", val);
+			lbl.Set("scale", new Vector2(val, val));
+
+			if (elapsed >= 1000 * maxTime) 
+			{
+				Teardown();
+			}
+		}
 	}
 
 	public override void _Input(InputEvent @event)
 	{
 		if (@event.IsActionPressed("debug_splutFX")) {
-			GD.Print("FIRE THE SPLUT!");
+			Setup();
 		}
 
 		base._Input(@event);
@@ -30,12 +44,14 @@ public partial class SplutFX : Node2D
 
 	private void Setup()
 	{
-		// Init FX to start anims
 		isRunning = true;
+		startTime = Time.GetTicksMsec();
+		lbl.Visible = true;
 	}
 	private void Teardown()
 	{
-		// Hide FX ready for next fire
+		lbl.Visible = false;
+		startTime = 0;
 		isRunning = false;
 	}
 }
