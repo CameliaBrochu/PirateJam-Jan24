fffdaeb61035bda939a9b226180362d8bfacb93d
Camélia Brochu <cameliabrochu@gmail.com>
2024-01-25 22:35:51 -0500

    introduce isSplutted

--- game/Bread/Enemy.cs
+++ game/Bread/Enemy.cs
@@ -4,15 +4,24 @@ using System.Runtime.InteropServices;
 
 public partial class Enemy : Area2D
 {
+	private bool isSplutted = false;
+	private AnimatedSprite2D splut;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		this.BodyEntered += _HandleCollision;
+		splut = this.GetNode<AnimatedSprite2D>("Splut");
+		splut.Frame = (int)GD.Randi() % 5;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		if (isSplutted)
+		{
+			splut.Visible = true;
+		}
+
 	}
 
 	public override void _ExitTree()
@@ -25,5 +34,7 @@ public partial class Enemy : Area2D
 	{
 		string path = other.GetPath().ToString();
 		GD.Print("AYO, collision avec un boulett ! path = ", path);
+		isSplutted = true;
+
 	}
 }
